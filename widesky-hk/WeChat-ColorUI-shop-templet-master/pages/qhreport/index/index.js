7faeb89b016afeda873698058a71af1b70f15eca
nickeraa <yi789456@163.com>
2025-03-25 16:22:48 +0800

    0325

--- widesky-hk/WeChat-ColorUI-shop-templet-master/pages/qhreport/index/index.js
+++ widesky-hk/WeChat-ColorUI-shop-templet-master/pages/qhreport/index/index.js
@@ -36,88 +36,18 @@ phone:'',
 
   onLoad: function (e) {
     var s = t.formatDate(new Date());
-    this.setData({ date: "2025-01-01", date2: s }),
-      e.p && this.setData({ p: e.p });
-    var i = this;
-    wx.getStorageSync("masterid")
-      ? wx.request({
-          url: a.globalData.api + "wx_sestore.ashx",
-          data: {},
-          header: { "content-type": "application/x-www-form-urlencoded" },
-          dataType: "json",
-          success: function (a) {
-            console.log(a);
-            a.data.push({
-              XF_STORECODE: "all",
-              XF_NAME: "所有店铺",
-              LCCODE: "所有店铺",
-            });
-            i.setData({ picker3: a.data });
-          },
-        })
-      : wx.request({
-          url: a.globalData.api + "checkstore.ashx",
-          data: { userid: wx.getStorageSync("userid") },
-          header: { "content-type": "application/x-www-form-urlencoded" },
-          dataType: "json",
-          success: function (a) {
-            console.log(a), i.setData({ picker3: a.data });
-          },
-        });
+    this.setData({ date: "2025-01-01", date2: s })
+   
+    
   },
-  bindPickerChange4: function (a) {
-    console.log("picker发送选择改变，携带值为", a.detail.value),
-      this.setData({
-        index4: a.detail.value,
-        xf_staffcode: this.data.picker4[a.detail.value].XF_STAFFCODE,
-      }),
-      console.log(this.data.xf_staffcode);
-  },
-  sestaff: function () {
-    (wx.getStorageSync("qguserid") || wx.getStorageSync("masterid")) &&
-      this.setData({ i: "0" });
-    var t = this;
-    wx.request({
-      url: a.globalData.api + "wx_sestaff.ashx",
-      data: {
-        xf_storecode: t.data.store,
-        userid: wx.getStorageSync("userid"),
-        i: t.data.i,
-      },
-      header: { "content-type": "application/x-www-form-urlencoded" },
-      dataType: "json",
-      success: function (a) {
-        console.log(a);
-        t.setData({ picker4: a.data }),
-          (wx.getStorageSync("qguserid") || wx.getStorageSync("masterid")) &&
-            (t.data.picker4.push({ XF_STAFFCODE: "all", XF_NAME: "所有员工" }),
-            t.setData({ picker4: t.data.picker4 })),
-          console.log(t.data.picker4);
-      },
-    });
-  },
+
+
   back: function () {
     wx.navigateBack({ delta: 0 });
   },
-  bindPickerChange3: function (a) {
-    console.log("picker发送选择改变，携带值为", a.detail.value),
-      this.setData({
-        index3: a.detail.value,
-        store: this.data.picker3[a.detail.value].XF_STORECODE,
-        storename: this.data.picker3[a.detail.value].XF_NAME,
-      }),
-      this.sestaff();
-  },
+
   onShow: function (a) {},
-  newsaleslist: function (a) {
-    wx.navigateTo({
-      url:
-        "/pages/scdeposit/index/index?xf_docno=" +
-        a.currentTarget.dataset.xf_docno +
-        "&tags=" +
-        a.currentTarget.dataset.tags,
-    });
-  },
+
 
   vip_sort: function (t) {
     var e = this;
